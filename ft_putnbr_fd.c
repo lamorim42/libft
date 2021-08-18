962a043113d6d51b7743c1c34389cd467de382ec
luishads <luishads@gmail.com>
2021-08-18 17:11:58 -0300

    update putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,27 +6,24 @@
 /*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/17 18:37:48 by lamorim           #+#    #+#             */
-/*   Updated: 2021/08/18 15:56:10 by lamorim          ###   ########.fr       */
+/*   Updated: 2021/08/18 17:02:24 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	int			sign;
 	long int	n_long;
 	char		digit;
 
 	n_long = n;
-	if (n < 0)
+	if (n_long < 0)
 	{
-		sign = -1;
-		write(1, "-", 1);
+		n_long = -n_long;
+		write(fd, "-", 1);
 	}
-	else
-		sign = 1;
-	n_long = n_long * sign;
 	if (n_long > 9)
 		ft_putnbr_fd(n_long / 10, fd);
 	digit = '0' + (n_long % 10);
