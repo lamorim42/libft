4b5b16ef9122652d3d1118209a51a908a3857dc6
luishads <luishads@gmail.com>
2021-09-28 15:38:57 -0300

    Refatored putstr

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/17 17:27:36 by lamorim           #+#    #+#             */
-/*   Updated: 2021/08/18 23:47:58 by lamorim          ###   ########.fr       */
+/*   Updated: 2021/09/28 15:34:39 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
+	size_t	len;
+
 	if (!s)
 		return ;
-	while (*s)
-		write (fd, s++, 1);
+	len = 0;
+	while (s[len])
+		len++;
+	write(fd, s, len);
 }
