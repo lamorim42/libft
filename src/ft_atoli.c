f69b511b01a065c98af0ebd266ebe97026b2e84b
luishads <luishads@gmail.com>
2022-03-16 14:14:06 -0300

    feat: add long atoi functio (aka atoli)

---
+++ src/ft_atoli.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoli.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/16 14:09:51 by lamorim           #+#    #+#             */
+/*   Updated: 2022/03/16 14:10:44 by lamorim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_is_space(int c);
+static int	ft_signal(int c);
+
+long int	ft_long_atoi(const char *nptr)
+{
+	int			i;
+	int			signal;
+	long int	nptr_int;
+
+	i = 0;
+	signal = 1;
+	nptr_int = 0;
+	while (ft_is_space(nptr[i]))
+		i++;
+	if (ft_signal(nptr[i]))
+	{
+		if (nptr[i] == '-')
+			signal *= -1;
+		i++;
+	}
+	while (ft_isdigit(nptr[i]))
+	{
+		nptr_int = (nptr_int * 10) + (nptr[i] - '0');
+		i++;
+	}
+	return (nptr_int * signal);
+}
+
+static int	ft_is_space(int c)
+{
+	if (c == '\t' || c == '\n' || c == '\v'
+		|| c == '\f' || c == '\r' || c == ' ')
+		return (1);
+	return (0);
+}
+
+static int	ft_signal(int c)
+{
+	if (c == '-' || c == '+')
+		return (1);
+	return (0);
+}
